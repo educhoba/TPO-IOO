80f28d968e04a75df4f7a6263f3d71b02345fdc1
Eduardo Chobadindegui <edu-choba@hotmail.com>
2022-06-01 21:39:00 -0300

    termine calcularExplosion()

--- src/Model/Submarino.java
+++ src/Model/Submarino.java
@@ -55,34 +55,65 @@ public class Submarino extends ObjetosJuego {
 		float mediaAltura = this.altura / 2.0f;
 		float medioLargo = this.altura / 2.0f;
 
-		float A = xSubmarino + medioLargo; // xVerticeDerecho
-		float B = xSubmarino - medioLargo; // xVerticeIzquierdo
-		float C = xSubmarino + mediaAltura;// yVerticeArriba
-		float D = xSubmarino - mediaAltura; // yVerticeAbajo
+		float A = xSubmarino - medioLargo; // xVerticeIzquierdo
+		float B = xSubmarino + medioLargo; // xVerticeDerecho
+		float C = ySubmarino + mediaAltura;// yVerticeArriba
+		float D = ySubmarino - mediaAltura; // yVerticeAbajo
 
-		// (A,C)          largo              (B,C)
-		// //////////////////////////////////
-		// //                              //
-		// //                              // alto
-		// //                              //
-		// //////////////////////////////////
-		// (A,D)                             (B,D)
+		// (A,C) (B,C)
+		// //////
+		// // // alto
+		// //////
+		// (A,D) (B,D)
 
-		// TODO: calcular distancia minima
-		float explosionDistancia = 0;
+		float explosionDistancia = calcularDistanciaMinima(A,B,C,D,xExplosion,yExplosion);
 
 		calcularPuntos(explosionDistancia);
 		calcularDaño(explosionDistancia);
 	}
 
+	private float calcularDistanciaMinima(float A, float B, float C, float D, float xExplosion, float yExplosion) {
+
+		float distanciaMinima = Float.MAX_VALUE;
+		float dAux = 0;
+		//Recorro x
+		for (float x = A; x <= B; x++) {
+			// DE (A,C) a (B,C)
+			dAux = calcularDistanciaPorPitagoras(x, C, xExplosion, yExplosion);
+			if (dAux < distanciaMinima)
+				distanciaMinima = dAux;
+			// DE (A,D) a (B,D)
+			dAux = calcularDistanciaPorPitagoras(x, D, xExplosion, yExplosion);
+			if (dAux < distanciaMinima)
+				distanciaMinima = dAux;
+		}
+		//Recorro y
+		for (float y = C; y <= D; y++) {
+			// DE (A,D) a (A,C)
+			dAux = calcularDistanciaPorPitagoras(A, y, xExplosion, yExplosion);
+			if (dAux < distanciaMinima)
+				distanciaMinima = dAux;
+			// DE (B,D) a (B,C)
+			dAux = calcularDistanciaPorPitagoras(B, y, xExplosion, yExplosion);
+			if (dAux < distanciaMinima)
+				distanciaMinima = dAux;
+		}
+
+		return distanciaMinima;
+	}
+
+	private float calcularDistanciaPorPitagoras(float x1, float y1, float x2, float y2) {
+		return (float) Math.sqrt(Math.pow(x2-x1,2) + Math.pow(y2-y1, 2));
+	}
+
 	private void calcularDaño(float explosionDistancia) {
-		//TODO ponerle los valores y las condiciones de la consigna
+		// TODO ponerle los valores y las condiciones de la consigna
 		boolean condicion1 = true;
 		boolean condicion2 = true;
 		int valor1 = 0;
 		int valor2 = 0;
 		int valor3 = 0;
-		
+
 		if (condicion1)
 			dañar(valor1);
 		else if (condicion2)
@@ -92,13 +123,13 @@ public class Submarino extends ObjetosJuego {
 	}
 
 	private void calcularPuntos(float explosionDistancia) {
-		//TODO ponerle los valores y las condiciones de la consigna
+		// TODO ponerle los valores y las condiciones de la consigna
 		boolean condicion1 = true;
 		boolean condicion2 = true;
 		int valor1 = 0;
 		int valor2 = 0;
 		int valor3 = 0;
-		
+
 		if (condicion1)
 			añadirPuntos(valor1);
 		else if (condicion2)
@@ -128,7 +159,7 @@ public class Submarino extends ObjetosJuego {
 		this.vidas++;
 		this.vidasExtrasContador++;
 	}
-	
+
 	public void pasarDeNivelEIncrementarDificultad(float porcentaje) {
 		ObjetosJuego.velocidadMultiplicador = ObjetosJuego.velocidadMultiplicador * (1 + (porcentaje / 100));
 		// TODO evento de nivel
